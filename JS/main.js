41b00fcb080eec760ae66e752616512d6b41941c
Amilton Junior <amilton.od@gmail.com>
2022-08-28 15:52:34 -0300

    first class

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+var nome = "Amilton Carvalho";
+var n1 = 29;
+var n2 = 30;
+//alert(nome + " tem " + idade + " anos.");
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 + n2);
+console.log(frase.toUpperCase()) // Colocar tudo maiúsculo
+console.log(frase.toLowerCase())// Colocar tudo minúscolo
