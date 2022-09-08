6f62b0cae7fd4d35b3bd450aa0dfbdf5bcf73115
Amilton Junior <amilton.od@gmail.com>
2022-09-08 11:28:27 -0300

    Aula arrays

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,31 @@
-var nome = "Amilton Carvalho";
-var n1 = 29;
-var n2 = 30;
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+
+
+
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["Maçã", "Pêra", "Laranja"];
+//lista.push("Uva"); // Inserir itens
+//lista.pop("Maçã"); Remover itens
+//console.log(lista);
+//console.log(lista.reverse())
+//console.log(lista.toString())
+//console.log(lista.join(" - "))
+
+//var nome = "Amilton Carvalho";
+//var n1 = 29;
+//var n2 = 30;
 //alert(nome + " tem " + idade + " anos.");
 //alert(idade + idade2);
-console.log(nome);
-console.log(n1 + n2);
-console.log(frase.toUpperCase()) // Colocar tudo maiúsculo
-console.log(frase.toLowerCase())// Colocar tudo minúscolo
+//console.log(nome);
+//console.log(n1 + n2);
+//console.log(frase.toUpperCase()) // Colocar tudo maiúsculo
+//console.log(frase.toLowerCase())// Colocar tudo minúscolo
